281650b19aae7dfccec82a84a405f7b7404d7114
OtamKtot <119840981+OtamKtot@users.noreply.github.com>
2023-08-05 00:49:02 +0300

    HW04_HoverTest

---
+++ src/test/java/gitHubHover/GitHoverTest.java
@@ -0,0 +1,22 @@
+package gitHubHover;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class GitHoverTest {
+@Test
+    void GitSolutionsHoverTest() {
+    //open page
+        open("https://github.com/");
+    //go to Enterprise
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+    //check up
+        $(".application-main h1").shouldHave(text("Build like the best"));
+        sleep(3000);
+    }
+}
